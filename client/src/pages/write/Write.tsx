1da3db00d2a630c583a8039f096c459e3e54d477
Mcdonalds Trump <whdmschd2@gmail.com>
2022-03-10 18:34:22 +0900

    Add: 편지 글쓰기 상태관리

--- client/src/pages/write/Write.tsx
+++ client/src/pages/write/Write.tsx
@@ -1,7 +1,44 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 
 const Write: FC = () => {
-  return <div>fdasfasdf</div>;
+  const [letterInfo, setLetterInfo] = useState({
+    title: '',
+    contents: '',
+  });
+
+  const handleChangeContents = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setLetterInfo({
+      ...letterInfo,
+      [name]: value,
+    });
+  };
+
+  const handleLetterSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(letterInfo);
+    setLetterInfo({
+      title: '',
+      contents: '',
+    });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleLetterSubmit}>
+        <input name="title" value={letterInfo.title} onChange={handleChangeContents} required placeholder="제목" />
+        <textarea
+          name="contents"
+          value={letterInfo.contents}
+          onChange={handleChangeContents}
+          required
+          placeholder="내용"
+        />
+        <button type="submit">send</button>
+      </form>
+    </div>
+  );
 };
 
 export default Write;
