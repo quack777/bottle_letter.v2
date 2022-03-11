3b024a868038ddf74df6b08a18443c7ad81264ff
Mcdonalds Trump <whdmschd2@gmail.com>
2022-03-11 19:39:59 +0900

    Add: server get mockdata

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,6 +7,32 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/build/index.html');
 });
 
+app.get('/letterInfo', (req, res) => {
+  res.send({
+    letterInfo: [
+      {
+        id: 1,
+        title: '첫번째 편지 제목',
+        contents: '첫번째 편지 내용',
+      },
+      {
+        id: 2,
+        title: '두번째 편지 제목',
+        contents: '두번째 편지 내용',
+      },
+      {
+        id: 3,
+        title: '세번째 편지 제목',
+        contents: '세번째 편지 내용',
+      },
+    ],
+  });
+});
+
+/* app.post('/writeLetter', (req, res) => {
+  const user = req.body;
+}); */
+
 app.listen(3000, () => {
   console.log('server is listening!');
 });
